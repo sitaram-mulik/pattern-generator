ad2896d4bb1a9ba1a3a8a361a55c2e2fbdfdd802
sitaram-mulik <sitaram99.race@gmail.com>
2025-05-17 12:58:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import PatternGen from './components/PatternGen'
+
+function App() {
+
+  return (
+    <div className='wrapper'>
+      <PatternGen />
+    </div>
+  )
+}
+
+export default App
